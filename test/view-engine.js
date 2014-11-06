b80e22728b7d299fcebdf13573486b691ac1ee61
Jean-Charles Sisk <jasisk@gmail.com>
2014-11-06 14:24:12 -0500

    make tests work with tape binary

--- test/view-engine.js
+++ test/view-engine.js
@@ -8,10 +8,11 @@ var fs = require('fs');
 var fixture = fs.readFileSync(path.resolve(__dirname, '../views/nested/view.dust'), {encoding: 'utf8'});
 var app = require('../');
 
+// note that this will only work if we listen once.
 var server = app.listen(0);
 
-app.on('start', function () {
-  test('adaro example', function (t) {
+test('adaro example', function (t) {
+  app.on('start', function () {
     var req = request(app);
 
     t.test('renders correctly', function (st) {
@@ -20,5 +21,5 @@ app.on('start', function () {
         st.end();
       });
     });
-  }).on('end', server.close.bind(server));
-});
+  });
+}).on('end', server.close.bind(server));
